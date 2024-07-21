5764edc4903b8fc94e922a3869beb1b44ab0527c
gioele-bernardini <gioele.g@outlook.com>
2024-07-21 03:12:37 +0200

    Test: Add unit test and cmakefile relative configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
-#include "../include/Neuron.hpp"
-
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-  Neuron *n = new Neuron(0.9);
-
-  cout << "Val: " << n->getVal() << endl;
-  cout << "Activated Val: " << n->getActivatedVal() << endl;
-  cout << "Derived Val: " << n->getDerivedVal() << endl;
+  cout << "Hello, World!\n" << endl;
 
   return 0;
 }
