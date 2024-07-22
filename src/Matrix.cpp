79f658bb83a0fad1f8044e0c4c23751bd8647368
gioele-bernardini <gioele.g@outlook.com>
2024-07-23 00:06:25 +0200

    Add: transpose method implemented.

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -29,6 +29,25 @@ double Matrix::generateRandomNumber() {
   return dis(gen);
 }
 
+void Matrix::setValue(int r, int c, double v) {
+  this->values.at(r).at(c) = v;
+}
+
+double Matrix::getValue(int r, int c) {
+  return this->values.at(r).at(c);
+}
+
+Matrix* Matrix::transpose() {
+  // isRandom false for the new Matrix, just setting up zero's for now
+  Matrix* m = new Matrix(this->numCols, this->numRows, false);
+
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+      m->setValue(i, j, this->getValue(j, i));
+    }
+  }
+}
+
 void Matrix::printToConsole() {
   for (int i = 0; i < numRows; i++) {
     for (int j = 0; j < numCols; j++) {
